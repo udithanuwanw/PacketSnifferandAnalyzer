721d6fdb21a007a5b3264e2a6eeeb67a1d8561e9
udithanuwanw <udithanuwanbandara2@gmail.com>
2025-02-20 21:10:24 +0530

    first commit

---
+++ src/IPv4.java
@@ -0,0 +1,83 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import jpcap.packet.IPPacket;
+import jpcap.packet.Packet;
+import jpcap.packet.TCPPacket;
+
+public class IPv4 extends PacketStructure {
+    /*A type of packet that uses JPCAP
+      for using underlaying IP Packets.
+    */
+
+    Hashtable properties = new Hashtable();
+    private final ArrayList<String> propertyLabels = new ArrayList<>(
+            Arrays.asList("Version", "TOS: Priority", "TOS: Throughput", "TOS: Reliability",
+                    "Length", "Identification", "Fragment: Don't Fragment", "Fragment: More Fragment",
+                    "Fragment Offset", "Time To Live", "Protocol", "Source IP", "Destination IP"));
+
+//,
+//"Source Host Name",
+//"Destination Host Name"));
+//	public String[] getValueNames(){
+//		return propertyLabels;
+//	}
+
+    @Override
+    public Boolean CheckPacketType(Packet p) {
+        if(p instanceof IPPacket &&
+                ((IPPacket)p).version == 4 &&
+                p instanceof TCPPacket)
+            return true;
+        else
+            return false;
+    }
+    @Override
+    public Object GetProperty(String label) {
+        return properties.get(label);
+    }
+    @Override
+    public Object[] GetProperties() {
+        Object[] property = new Object[propertyLabels.size()];
+        for(int i = 0; i < propertyLabels.size(); i++) {
+            property[i] = properties.get(propertyLabels.get(i));
+        }
+        return property;
+    }
+    @Override
+    public ArrayList<String> GetPropertiesLabels() {
+        return propertyLabels;
+    }
+    @Override
+    public String GetProtocolLabel() {
+        return "IPv4";
+    }
+    @Override
+    public String GetSrcAdrs() {
+        return properties.get("Source IP").toString();
+    }
+    @Override
+    public String GetDestAdrs() {
+        return properties.get("Destination IP").toString();
+    }
+    @Override
+    public void SetProperties(Packet packet) {
+        IPPacket ip = (IPPacket) packet;
+        properties.put(propertyLabels.get(0), 4);
+        properties.put(propertyLabels.get(1), (int) ip.priority);
+        properties.put(propertyLabels.get(2), ip.t_flag);
+        properties.put(propertyLabels.get(3), ip.r_flag);
+        properties.put(propertyLabels.get(4), (int) ip.length);
+        properties.put(propertyLabels.get(5), ip.ident);
+        properties.put(propertyLabels.get(6), ip.dont_frag);
+        properties.put(propertyLabels.get(7), ip.more_frag);
+        properties.put(propertyLabels.get(8), (int) ip.offset);
+        properties.put(propertyLabels.get(9), (int) ip.hop_limit);
+        properties.put(propertyLabels.get(10), (int) ip.protocol);
+        properties.put(propertyLabels.get(11), ip.src_ip.getHostAddress());
+        properties.put(propertyLabels.get(12), ip.dst_ip.getHostAddress());
+    }
+
+}
+
